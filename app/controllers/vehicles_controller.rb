ee4afe5a486c8eca52bf8d7b1a96a4ee6bce0768
NoePigeau <npigeau@myges.fr>
2022-05-28 11:39:16 +0200

    feat(vehicle): new form create

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -7,4 +7,26 @@ class VehiclesController < ActionController::Base
     def show
         @vehicle = Vehicle.find(params[:id])
     end
+
+    def new
+        @vehicle = Vehicle.new
+    end
+    
+    def create
+        @vehicle = Vehicle.new(params_vehicle)
+
+        if @vehicle.save
+            redirect_to vehicles_path        
+        else
+            render :new   
+        end
+
+        
+    end
+
+    private 
+
+    def params_vehicle
+        params.require(:vehicle).permit(:name, :consumption)
+    end
 end
